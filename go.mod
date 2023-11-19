f6efec4596d969e00d077d496e322ffa77e2c68f
dillonstreator <dillonstreator@gmail.com>
2023-11-18 19:19:42 -0600

    panic recovery logging includes trace id and request id

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/dillonstreator/go-chi
 go 1.21.3
 
 require (
+	github.com/dillonstreator/opentelemetry-go-contrib/instrumentation/net/http/otelhttp v0.0.0-20231119004728-1e3363d236ad
 	github.com/go-chi/chi v1.5.5
+	github.com/google/uuid v1.4.0
 	go.opentelemetry.io/otel v1.21.0
-	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v0.44.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.21.0
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.21.0
 	go.opentelemetry.io/otel/sdk v1.21.0
-	go.opentelemetry.io/otel/sdk/metric v1.21.0
+	go.opentelemetry.io/otel/trace v1.21.0
 )
 
 require (
@@ -17,13 +19,9 @@ require (
 	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/uuid v1.4.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.46.1 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.21.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.21.0 // indirect
 	go.opentelemetry.io/otel/metric v1.21.0 // indirect
-	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
--- main.go
+++ main.go
@@ -13,10 +13,13 @@ import (
 	"syscall"
 	"time"
 
+	// NOTE: github.com/dillonstreator/opentelemetry-go-contrib/instrumentation/net/http/otelhttp is required
+	// until https://github.com/open-telemetry/opentelemetry-go-contrib/pull/4591 is reviewed/merged
+	// to ensure panic recovery logging includes request id and trace id
+	"github.com/dillonstreator/opentelemetry-go-contrib/instrumentation/net/http/otelhttp"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/google/uuid"
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -30,13 +33,14 @@ func main() {
 
 	otelShutdown, err := setupOTelSDK(context.Background(), cfg)
 	if err != nil {
-		logger.Error("Setting up open telemetry", "err", err)
+		logger.Error("Setting up open telemetry", slog.Any("error", err))
 		os.Exit(1)
 	}
 
-	r := chi.NewMux()
-
-	r.Use(func(h http.Handler) http.Handler {
+	mux := chi.NewMux()
+	mux.Use(middleware.Recoverer)
+	mux.Use(otelhttp.NewMiddleware("chi"))
+	mux.Use(func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
 
@@ -54,7 +58,12 @@ func main() {
 			ww := middleware.NewWrapResponseWriter(w, 0)
 			rc := newByteReadCloser(r.Body)
 			r.Body = rc
-			h.ServeHTTP(ww, setLoggerReq(r, l))
+
+			// overwrite `r`'s memory so that recoverer can access the log entry
+			*r = *setLogger(r, l)
+			*r = *middleware.WithLogEntry(r, newLogEntry(l))
+
+			h.ServeHTTP(ww, r)
 
 			l.Info(
 				"Request handled",
@@ -71,24 +80,28 @@ func main() {
 		})
 	})
 
-	r.Get(cfg.healthEndpoint, func(w http.ResponseWriter, r *http.Request) {
+	mux.Get(cfg.healthEndpoint, func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	r.Get("/hi", func(w http.ResponseWriter, r *http.Request) {
-		l := getLoggerReq(r)
+	mux.Get("/hi", func(w http.ResponseWriter, r *http.Request) {
+		l := getLogger(r)
 		l.Info("hi")
 		w.Write([]byte("hi"))
 	})
 
+	mux.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("testing panic recovery and logging")
+	})
+
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", cfg.port),
-		Handler: otelhttp.NewHandler(r, "chi"),
+		Handler: mux,
 	}
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			logger.Error("Server error", "err", err)
+			logger.Error("Server error", slog.Any("error", err))
 			os.Exit(1)
 		}
 	}()
@@ -106,13 +119,13 @@ func main() {
 
 	err = srv.Shutdown(ctx)
 	if err != nil {
-		logger.Error("Server shutdown", "err", err)
+		logger.Error("Server shutdown", slog.Any("error", err))
 		os.Exit(1)
 	}
 
 	err = otelShutdown(ctx)
 	if err != nil {
-		logger.Error("Open telemetry shutdown", "err", err)
+		logger.Error("Open telemetry shutdown", slog.Any("error", err))
 		os.Exit(1)
 	}
 }
@@ -146,10 +159,27 @@ const (
 	ctxKeyLogger ctxKey = "logger"
 )
 
-func getLoggerReq(r *http.Request) *slog.Logger {
+func getLogger(r *http.Request) *slog.Logger {
 	return r.Context().Value(ctxKeyLogger).(*slog.Logger)
 }
 
-func setLoggerReq(r *http.Request, l *slog.Logger) *http.Request {
+func setLogger(r *http.Request, l *slog.Logger) *http.Request {
 	return r.WithContext(context.WithValue(r.Context(), ctxKeyLogger, l))
+}
+
+type logEntry struct {
+	logger *slog.Logger
+}
+
+var _ middleware.LogEntry = (*logEntry)(nil)
+
+func newLogEntry(logger *slog.Logger) *logEntry {
+	return &logEntry{logger}
+}
+
+func (l *logEntry) Panic(v interface{}, stack []byte) {
+	l.logger.Error("panic caught", slog.Any("panic", v), slog.String("stack", string(stack)))
+}
+
+func (l *logEntry) Write(status int, bytes int, header http.Header, elapsed time.Duration, extra interface{}) {
 }
